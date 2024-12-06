575dc858a9eb47610142b13c026df60615cd1207
Leone Hollanda <leone.hollanda@irede.org.br>
2024-12-06 11:09:45 -0300

    refactor translation

--- utils/translations.ts
+++ utils/translations.ts
@@ -1,23 +1,5 @@
 const translations = {
-  en: {
-    home: 'Home',
-    about: 'About',
-    projects: 'Projects',
-    contact: 'Contact',
-    heroTitle: 'Hello, my name is Leone Hollanda. ',
-    heroSubtitle: "Welcome to my portfolio",
-    aboutTitle: 'About Me',
-    aboutContent1: 'Hello, my name is Leone. I am currently in the seventh semester of my Computer Engineering degree at the Federal Institute of Education, Science, and Technology of Ceará (IFCE). I consider myself a junior Full Stack developer and strive to continuously improve my skills by learning more and more.',
-    aboutContent2: 'I joined the LIT laboratory as a volunteer and had the opportunity to gain incredible experiences. Throughout my journey, I had mentors whom I still look up to today, which motivates me to achieve my goal of becoming a software engineer.', projectsTitle: 'My Projects',
-    contactTitle: 'Contact Me',
-    footerText: 'All rights reserved.',
-    experienceTitle: 'Experience',
-    experienceDescription: '3 years working as a Software Developer in companies such as:',
-    experienceItem1: 'LIT: Worked as a back-end developer using Java to define the software architecture. Responsible for class modeling, relationship mapping, UML diagrams, and contributing to strategic decisions on system structure and functionality.',
-    experienceItem2: "Irede: I worked with automations using C# for the Azure DevOps platform and participated in a project using Java 17. I developed a Spring Boot application with Java, using a RESTful architecture to create scalable APIs. The system was designed for ERP and integrated with a  PostgreSQL database using JPA/Hibernate for data persistence. I implemented automated tests with JUnit and used Maven for dependency management.",
-    experienceItem3: 'Salvus: Worked as a full-stack developer, using Node.js and React.js for web applications, JUnit for automated testing, and gained experience with AWS, utilizing Lambda back-end with API Gateway.',
-    years: 'years',
-  },
+
   pt: {
     home: 'Início',
     about: 'Sobre',
@@ -37,7 +19,25 @@ const translations = {
     experienceItem2: "Irede: Trabalhei com automações usando C# para a plataforma Azure DevOps e participei de um projeto usando Java 17,Desenvolvi uma aplicação em Spring Boot com Java, utilizando arquitetura RESTful para criar APIs escaláveis. O sistema foi projetado para ERP e integrado a um banco de dados PostgreSQL com JPA/Hibernate para persistência de dados. Implementei testes automatizados com JUnit e utilizei Maven para o gerenciamento de dependências",
     experienceItem3: 'Salvus: Atuei como full stack, usando Node.js e React.js para aplicações web, JUnit para testes automatizados e tive experiência com AWS, utilizando back-end Lambda junto com API Gateway.',
     years: 'anos',
-  },
+  }, en: {
+    home: 'Home',
+    about: 'About',
+    projects: 'Projects',
+    contact: 'Contact',
+    heroTitle: 'Hello, my name is Leone Hollanda. ',
+    heroSubtitle: "Welcome to my portfolio",
+    aboutTitle: 'About Me',
+    aboutContent1: 'Hello, my name is Leone. I am currently in the seventh semester of my Computer Engineering degree at the Federal Institute of Education, Science, and Technology of Ceará (IFCE). I consider myself a junior Full Stack developer and strive to continuously improve my skills by learning more and more.',
+    aboutContent2: 'I joined the LIT laboratory as a volunteer and had the opportunity to gain incredible experiences. Throughout my journey, I had mentors whom I still look up to today, which motivates me to achieve my goal of becoming a software engineer.', projectsTitle: 'My Projects',
+    contactTitle: 'Contact Me',
+    footerText: 'All rights reserved.',
+    experienceTitle: 'Experience',
+    experienceDescription: '3 years working as a Software Developer in companies such as:',
+    experienceItem1: 'LIT: Worked as a back-end developer using Java to define the software architecture. Responsible for class modeling, relationship mapping, UML diagrams, and contributing to strategic decisions on system structure and functionality.',
+    experienceItem2: "Irede: I worked with automations using C# for the Azure DevOps platform and participated in a project using Java 17. I developed a Spring Boot application with Java, using a RESTful architecture to create scalable APIs. The system was designed for ERP and integrated with a  PostgreSQL database using JPA/Hibernate for data persistence. I implemented automated tests with JUnit and used Maven for dependency management.",
+    experienceItem3: 'Salvus: Worked as a full-stack developer, using Node.js and React.js for web applications, JUnit for automated testing, and gained experience with AWS, utilizing Lambda back-end with API Gateway.',
+    years: 'years',
+  }
 }
 
 export default translations
